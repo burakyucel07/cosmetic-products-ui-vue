5c26b03edac8dd40699c4fab0242ca56e3e247ed
Burak Yücel <burakyucel@outlook.com.tr>
2023-02-20 22:26:56 +0300

    Add general Product view

---
+++ src/views/ProductView.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="mx-20">
+    <div class="text-2xl text-center bg-slate-300 py-5 px-10">
+      We fight animal testing!
+    </div>
+    <div
+      class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 mt-10"
+    >
+      <div
+        class="col-span-1 mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+      ></div>
+      <div class="px-10 col-span-2">
+        <h2 class="text-2xl">Cucumber Watermelon</h2>
+        <h5 class="text-lg">Shower Gel</h5>
+        <p class="py-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
+          magna molestie, gravida diam in, faucibus urna. Duis elit ipsum,
+          imperdiet pharetra lacinia at, consequat a sapien. In commodo accumsan
+          accumsan. Fusce eu facilisis odio, in viverra diam. Proin ullamcorper
+          eleifend risus, a efficitur mauris vulputate vitae.
+        </p>
+        <span class="text-bold text-3xl pt-1">$10.50</span>
+        <span class="text-slate-500 px-2">for</span
+        ><span class="font-bold text-lg">250g</span>
+        <div class="my-5 flex">
+          <button
+            class="px-4 pb-1 bg-slate-600 rounded-full text-white text-4xl"
+          >
+            -
+          </button>
+          <input type="number" min="0" class="text-4xl mx-2 w-40" />
+          <button
+            class="px-4 pb-1 bg-slate-600 rounded-full text-white text-4xl"
+          >
+            +
+          </button>
+          <button
+            class="px-8 py-2 ml-5 bg-slate-600 rounded-full text-xl text-white"
+          >
+            Add to Bag
+          </button>
+        </div>
+        <div class="mt-8">
+          <h5 class="mb-2 text-xl">Certifications</h5>
+          <div class="w-24 h-12 rounded-lg bg-slate-400"></div>
+        </div>
+        <div class="mt-8">
+          <h5 class="mb-2 text-xl">Ingredients</h5>
+          <p>
+            Sodium Laureth Sulfate, Glycerine, Sodium Cocoamphoacetate, Water
+            (Aqua), Fresh Wheatgrass Juice, Lauryl Betaine, Chlorophyllin Water,
+            Perfume, Sandalwood Oil, Bergamot Oil, Neroli Oil, Fine Sea Salt,
+            Coumarin, Benzyl Benzoate, Limonene, Linalool, Alpha-Isomethyl
+            Ionone.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="my-20">
+      <div class="h-0.5 bg-stone-200"></div>
+    </div>
+
+    <div
+      class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 xl:grid-cols-4 mt-10"
+    >
+      <div class="col-span-4">
+        <h2 class="text-2xl">Reviews</h2>
+        <div class="flex">
+          <svg
+            class="w-8 h-8 flex-inline"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              clip-rule="evenodd"
+              fill-rule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+            ></path>
+          </svg>
+          <svg
+            class="w-8 h-8 inline"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            ></path>
+          </svg>
+          <svg
+            class="w-8 h-8 inline"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            ></path>
+          </svg>
+          <svg
+            class="w-8 h-8 inline"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            ></path>
+          </svg>
+          <svg
+            class="w-8 h-8 inline"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            ></path>
+          </svg>
+          <div class="leading-[2rem] mx-3">|</div>
+          <div class="leading-[2.1rem]">Total 45 reviews</div>
+          <div>
+            <div
+              class="border-solid border-2 border-stone-500 rounded-lg px-2 py-1 bg-white flex"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                ></path>
+              </svg>
+              <input type="text" placeholder="Search text in reviews" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
