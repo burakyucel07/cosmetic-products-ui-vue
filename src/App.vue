5657628307f3ce492b88c234462233f946be9676
Burak Yücel <burakyucel@outlook.com.tr>
2023-02-20 22:23:50 +0300

    Add footer to all pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <MainNavigation />
-  <div class="container mx-auto px-2"><RouterView /></div>
+  <div class="container mx-auto px-2 pt-6">
+    <RouterView />
+    <div
+      class="max-w-full mx-auto border-solid border-t-2 border-t-stone-200 mt-20"
+    >
+      <div class="mb-10 mt-20 text-center">Copyright - 2023</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
