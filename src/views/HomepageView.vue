31ba921be3ad07b486cf184678de0daa3abea1a5
Burak Yücel <burakyucel@outlook.com.tr>
2023-02-20 22:26:18 +0300

    Add products section

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="pt-4">
-    <h1 class="text-4xl mb-3">Cosmetic Shop</h1>
-    <p class="text-lg mb-6">
-      Cosmetic shop has a great variation of cosmetic products. Cleaning,
-      scents, moisturizers, and more. Please don't hesitate to check our
-      products.
-    </p>
-  </div>
+  <h1 class="text-4xl mb-3">Cosmetic Shop</h1>
+  <p class="text-lg mb-6">
+    Cosmetic shop has a great variation of cosmetic products. Cleaning, scents,
+    moisturizers, and more. Please don't hesitate to check our products.
+  </p>
 
   <div class="pt-12 pr-8 pb-12 pl-8 bg-slate-200 text-gray-900 my-8 mx-2">
     <h1 class="text-4xl font-bold mb-4">We hate animal testing!</h1>
@@ -25,17 +22,48 @@
 
   <h1 class="text-4xl mb-3">Our Newly Added Products</h1>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    <div
-      class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4"
-    ></div>
-    <div
-      class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4"
-    ></div>
-    <div
-      class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4"
-    ></div>
-    <div
-      class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4"
-    ></div>
+    <div>
+      <div
+        class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+      ></div>
+      <div class="text-center pb-4">
+        <h2 class="text-2xl">Cucumber Watermelon</h2>
+        <h5 class="text-lg">Shower Gel</h5>
+        <div class="text-bold text-3xl pt-1">$10.50</div>
+      </div>
+    </div>
+
+    <div>
+      <div
+        class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+      ></div>
+      <div class="text-center pb-4">
+        <h2 class="text-2xl">Cucumber Watermelon</h2>
+        <h5 class="text-lg">Shower Gel</h5>
+        <div class="text-bold text-3xl pt-1">$10.50</div>
+      </div>
+    </div>
+
+    <div>
+      <div
+        class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+      ></div>
+      <div class="text-center pb-4">
+        <h2 class="text-2xl">Cucumber Watermelon</h2>
+        <h5 class="text-lg">Shower Gel</h5>
+        <div class="text-bold text-3xl pt-1">$10.50</div>
+      </div>
+    </div>
+
+    <div>
+      <div
+        class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+      ></div>
+      <div class="text-center pb-4">
+        <h2 class="text-2xl">Cucumber Watermelon</h2>
+        <h5 class="text-lg">Shower Gel</h5>
+        <div class="text-bold text-3xl pt-1">$10.50</div>
+      </div>
+    </div>
   </div>
 </template>
