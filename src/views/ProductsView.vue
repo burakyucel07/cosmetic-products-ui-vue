d1ac74f9cb4f752484ae54d0885f08a574744ad2
Burak Yücel <burakyucel@outlook.com.tr>
2023-02-20 22:26:41 +0300

    Add Products view

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,3 +1,55 @@
 <template>
-    <div></div>
+  <div>
+    <h1 class="text-5xl mb-3">All Products of Ours</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div>
+        <div
+          class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+        ></div>
+        <div class="text-center pb-4">
+          <h2 class="text-2xl">Cucumber Watermelon</h2>
+          <h5 class="text-lg">Shower Gel</h5>
+          <div class="text-bold text-3xl pt-1">$10.50</div>
+        </div>
+      </div>
+
+      <div>
+        <div
+          class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+        ></div>
+        <div class="text-center pb-4">
+          <h2 class="text-2xl">Cucumber Watermelon</h2>
+          <h5 class="text-lg">Shower Gel</h5>
+          <div class="text-bold text-3xl pt-1">$10.50</div>
+        </div>
+      </div>
+
+      <div>
+        <div
+          class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+        ></div>
+        <div class="text-center pb-4">
+          <h2 class="text-2xl">Cucumber Watermelon</h2>
+          <h5 class="text-lg">Shower Gel</h5>
+          <div class="text-bold text-3xl pt-1">$10.50</div>
+        </div>
+      </div>
+
+      <div>
+        <div
+          class="mx-3 my-3 bg-gradient-to-b from-white to-slate-300 rounded-lg py-4 px-4 h-80"
+        ></div>
+        <div class="text-center pb-4">
+          <h2 class="text-2xl">Cucumber Watermelon</h2>
+          <h5 class="text-lg">Shower Gel</h5>
+          <div class="text-bold text-3xl pt-1">$10.50</div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center py-10">
+      <button class="px-16 py-2 bg-slate-600 rounded-full text-xl text-white">
+        Load More
+      </button>
+    </div>
+  </div>
 </template>
