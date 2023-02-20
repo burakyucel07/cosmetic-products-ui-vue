e1fbd22db079ead00c6ecef84c2319b9b373e83c
Burak Yücel <burakyucel@outlook.com.tr>
2023-02-20 22:25:30 +0300

    Init About view

---
+++ src/views/AboutView.vue
@@ -0,0 +1,79 @@
+<template>
+  <h1 class="text-5xl mb-3">About Cosmetic Shop</h1>
+
+  <p class="mb-10">
+    Cosmetic shop has a great variation of cosmetic products. Cleaning, scents,
+    moisturizers, and more. Please don’t hesitate to check our products.
+  </p>
+
+  <div>
+    <h2 class="text-4xl mb-3">How do Detergents Work?</h2>
+    <p class="mb-10">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget
+      consequat metus. Nam sagittis eros sit amet lacinia placerat. Pellentesque
+      tellus libero, scelerisque id lobortis eu, semper vel nulla. Nulla dapibus
+      mattis scelerisque. Nunc porttitor dignissim metus sit amet varius.
+      Vestibulum varius pellentesque quam, ut volutpat lectus vehicula sed. Sed
+      aliquet nibh vel sem viverra elementum. Aenean mattis ornare fringilla.
+      Nulla tincidunt finibus odio ac venenatis. Curabitur at tristique erat.
+      Etiam id lacus ac nisl vestibulum auctor vel sit amet leo. Ut faucibus,
+      libero nec laoreet imperdiet, mauris metus elementum sapien, vitae ornare
+      elit dui a purus. Morbi hendrerit cursus varius. Praesent sollicitudin
+      tincidunt erat at aliquam. Phasellus vitae aliquam libero. Nulla aliquet,
+      turpis ut maximus finibus, enim tellus suscipit purus, id fermentum enim
+      turpis vitae massa.
+    </p>
+    <p class="mb-10">
+      Integer et ante ex. Donec laoreet elit vel tortor interdum posuere. Aenean
+      bibendum, justo et pellentesque condimentum, lacus erat porta dolor, ac
+      pulvinar lacus nisl nec nisi. In laoreet tempus augue, non commodo mi
+      imperdiet eu. Duis pellentesque condimentum risus, at porttitor erat.
+      Fusce sollicitudin, diam eu condimentum varius, leo lorem posuere purus,
+      vitae auctor orci augue eu ipsum. Integer elementum quam vitae augue
+      venenatis efficitur. Praesent placerat sollicitudin tortor eu vestibulum.
+      Praesent pharetra, ipsum non tempus laoreet, mi leo fringilla lorem, et
+      placerat dolor tortor vel dui. Vivamus vulputate lorem eu dui hendrerit,
+      eget feugiat lacus pharetra.
+    </p>
+  </div>
+
+  <div>
+    <h2 class="text-4xl mb-3">Is Methylparaben bad?</h2>
+    <p class="mb-10">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget
+      consequat metus. Nam sagittis eros sit amet lacinia placerat. Pellentesque
+      tellus libero, scelerisque id lobortis eu, semper vel nulla. Nulla dapibus
+      mattis scelerisque. Nunc porttitor dignissim metus sit amet varius.
+      Vestibulum varius pellentesque quam, ut volutpat lectus vehicula sed. Sed
+      aliquet nibh vel sem viverra elementum. Aenean mattis ornare fringilla.
+      Nulla tincidunt finibus odio ac venenatis. Curabitur at tristique erat.
+      Etiam id lacus ac nisl vestibulum auctor vel sit amet leo. Ut faucibus,
+      libero nec laoreet imperdiet, mauris metus elementum sapien, vitae ornare
+      elit dui a purus. Morbi hendrerit cursus varius. Praesent sollicitudin
+      tincidunt erat at aliquam. Phasellus vitae aliquam libero. Nulla aliquet,
+      turpis ut maximus finibus, enim tellus suscipit purus, id fermentum enim
+      turpis vitae massa.
+    </p>
+  </div>
+
+  <div>
+    <h2 class="text-4xl mb-3">
+      How do We Source Green Colorings in Our Products?
+    </h2>
+    <p class="mb-10">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget
+      consequat metus. Nam sagittis eros sit amet lacinia placerat. Pellentesque
+      tellus libero, scelerisque id lobortis eu, semper vel nulla. Nulla dapibus
+      mattis scelerisque. Nunc porttitor dignissim metus sit amet varius.
+      Vestibulum varius pellentesque quam, ut volutpat lectus vehicula sed. Sed
+      aliquet nibh vel sem viverra elementum. Aenean mattis ornare fringilla.
+      Nulla tincidunt finibus odio ac venenatis. Curabitur at tristique erat.
+      Etiam id lacus ac nisl vestibulum auctor vel sit amet leo. Ut faucibus,
+      libero nec laoreet imperdiet, mauris metus elementum sapien, vitae ornare
+      elit dui a purus. Morbi hendrerit cursus varius. Praesent sollicitudin
+      tincidunt erat at aliquam. Phasellus vitae aliquam libero. Nulla aliquet,
+      turpis ut maximus finibus, enim tellus suscipit purus, id fermentum enim
+      turpis vitae massa.
+    </p>
+  </div>
+</template>
