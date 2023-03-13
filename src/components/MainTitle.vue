ebc80fb94790785978d8cd1618af6cd385972721
Burak Yücel <burakyucel@outlook.com.tr>
2023-03-13 20:35:36 +0300

    Add title component

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-4xl mb-3">{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainTitle",
+  props: ["title"],
+});
+</script>
